e540e538406cf283553702edc4725856c51ee0e7
Jonathan Thorne <jonathanthorne13@gmail.com>
2019-12-28 19:14:35 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='charmedcurses',
+      version='0.1',
+      description='Another curses package',
+      url='https://github.com/chillhumanoid/charmedcurses',
+      author='chillhumanoid',
+      author_email='contact@theunquenchedservant.com',
+      license='MIT',
+      packages=['charmedcurses'],
+      zip_safe=False)
